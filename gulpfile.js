7a98e78714829ac1bc16b1699e4182efc4b595ba
Guilherme Cardoso <email@guilhermecardoso.pt>
2015-08-27 20:34:20 +0100

    fixed the pi sass implementation

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,14 @@ var gulp = require('gulp'),
 
 var sassDir = ['./src/*.scss', './src/**/*.scss', './src/**/**/*.scss'];
 
-gulp.task('scripts', function(){
-   gulp.src(['./web/bower/pi-angular/dist/pi-angular.js'])
-      .pipe(concat('pi-angular.js'))
-      .pipe(gulp.dest('./web/public/dist'));
-
-    gulp.src(['./web/bower/angular/angular.js'])
-      .pipe(concat('angular.js'))
-      .pipe(gulp.dest('./web/public/dist'));
-});
 gulp.task('sass', function () {
     gulp.src(sassDir)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./release'));
 });
-gulp.task('default', ['sass', 'scripts']);
+
 gulp.task('watch', function(){
     gulp.watch(sassDir, ['sass']);
 });
+
+gulp.task('default', ['sass']);
